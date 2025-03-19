30c3bcea37d7419efbc510e1753792fa8a3732b6
Iheukwumerevivian23 <nneomavivian2002@gmail.com>
2025-03-19 10:53:59 +0100

    Updated calculator project

--- calculator.py
+++ calculator.py
@@ -1,24 +1,23 @@
-
 # create a function for multiplication
 def multiply(num1, num2):
     return num1 * num2
-# # Create a function to divide 
+# Create a function to divide
 def divide(num1, num2):
     if num2 == 0:
         print("Error: Division by zero is not allowed.")
         return None
     return num1 / num2
-# # Create a function to add
+# Create a function to add
 def addition(num1, num2):
     return num1 + num2
-
+# Create a function to substract
 def subtract(num1, num2):
     return num1 - num2
 
 def calculator():
     result = None
     while True:
-        print("\nWelcome to my calculator")
+        print("Welcome to my calculator")
         print("Please choose an operation:")
         print("1. Addition", "\n2. Multiplication", "\n3. Division", "\n4. Subtraction")
         choice = input("Please choose an operation (1-4): ")
@@ -29,14 +28,19 @@ def calculator():
         
         if result is None:
             num1 = int(input("First Number: "))
+            num2 = int(input("Second Number: "))
         else:
-            use_prev = input(f"Do you want to use the previous result ({result}) as the first number? (yes/no): ").strip().lower()
-            if use_prev == "yes":
+            use_prev = input(f"Do you want to use the previous result ({result})? (first/second/no): ").strip().lower()
+            
+            if use_prev == "first":
                 num1 = result
+                num2 = int(input("Second Number: "))
+            elif use_prev == "second":
+                num1 = int(input("First Number: "))
+                num2 = result
             else:
                 num1 = int(input("First Number: "))
-        
-        num2 = int(input("Second Number: "))
+                num2 = int(input("Second Number: "))
         
         if choice == "1":
             result = addition(num1, num2)
